e7c559db99a7b89d9a34483194d9c98b9f223f1b
guorunze <guorunze@kanyun.com>
2026-02-28 14:31:09 +0800

    FEAT:add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { useGameStore } from './store/gameStore';
 import HomePage from './components/HomePage';
 import GameBoard from './components/GameBoard';
@@ -5,9 +6,10 @@ import GameBoard from './components/GameBoard';
 export default function App() {
   const phase = useGameStore((s) => s.phase);
 
-  if (phase === 'idle') {
-    return <HomePage />;
-  }
-
-  return <GameBoard />;
+  return (
+    <>
+      {phase === 'idle' ? <HomePage /> : <GameBoard />}
+      <Analytics />
+    </>
+  );
 }
